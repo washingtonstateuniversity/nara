f2451c89450ff32103434243e65e58d1da6d48f7
Phil Cable <pcable@wsu.edu>
2016-11-04 11:42:08 -0700

    Remove `before-main.php`, move the spine call into `before-binder.php`

--- parts/before-binder.php
+++ parts/before-binder.php
@@ -2,17 +2,5 @@
 	<div id="nara-logo">
 		<a href="<?php echo esc_url( home_url() ); ?>"><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/logo.png' ); ?>" ></a>
 	</div>
-	<?php
-	$spine_site_args = array(
-		'theme_location'  => 'site',
-		'menu'            => 'site',
-		'container'       => false,
-		'container_class' => false,
-		'container_id'    => false,
-		'menu_class'      => 'main-menu',
-		'menu_id'         => null,
-		'items_wrap'      => '<ul>%3$s</ul>',
-		'depth'           => 3,
-	);
-	wp_nav_menu( $spine_site_args ); ?>
+	<?php get_template_part( 'spine' ); ?>
 </nav>
