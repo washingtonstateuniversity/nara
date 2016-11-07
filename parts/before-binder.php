1fe9c7d1a9aebffb38bb472ed2220c6ff916855c
Phil Cable <pcable@wsu.edu>
2016-11-07 15:55:32 -0800

    Add back `before-main.php`, remove `before-binder.php`
    This is so the mobile nav picks up the default Spine styles

--- parts/before-binder.php
+++
@@ -1,6 +0,0 @@
-<nav role="navigation">
-	<div id="nara-logo">
-		<a href="<?php echo esc_url( home_url() ); ?>"><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/logo.png' ); ?>" ></a>
-	</div>
-	<?php get_template_part( 'spine' ); ?>
-</nav>
