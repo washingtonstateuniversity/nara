12cf9df5c6595f56c8b34e90c88b4f043377bd03
Charles J Dillon <charles.dillon2@wsu.edu>
2016-11-09 13:40:48 -0800

    Adds footer to stick to the bottom regardless of content height

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ add_filter( 'spine_child_theme_version', 'nara_theme_version' );
  * @return string
  */
 function nara_theme_version() {
-	return '0.0.14';
+	return '0.0.15';
 }
 
 add_filter( 'wsuwp_uc_people_to_add_to_content', 'nara_modify_uc_object_people_content', 10, 1 );
